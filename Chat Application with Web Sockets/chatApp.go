29187beb59c8753b68bcdc7d6ff8adce3cce0794
Rohan <rohanali@live.com>
2020-06-18 16:15:11 -0700

    Templates Added

--- Chat Application with Web Sockets/chatApp.go
+++ Chat Application with Web Sockets/chatApp.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request for the template.
+func (temp *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if temp.templ == nil {
+		temp.templ = template.Must(template.ParseFiles(filepath.Join("templates", temp.filename)))
+	}
+	temp.templ.Execute(w, nil)
+
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //res,req on root page
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>Chat Room</title>
-        </head>
-        <body>
-          Let's chat!
-        </body>
-      </html>
-    `))
-	})
+	http.Handle("/", &templateHandler{filename: "/index.html"})
 
 	// start the web servers
 	if err := http.ListenAndServe(":3000", nil); err != nil { //Defining Port
