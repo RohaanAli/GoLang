87f82117fb05d936471e737d72c9a988aec6a2b2
Rohan <rohanali@live.com>
2020-06-18 17:05:07 -0700

    Adding Select for sync and two more channel + map

--- Chat Application with Web Sockets/room.go
+++ Chat Application with Web Sockets/room.go
@@ -1,8 +1,25 @@
 package main
 
 type room struct {
-	forward chan []byte
+	forward chan []byte // forward is a channel that holds incoming messages that should be forwarded to the other clients.
+
+	join    chan *client     //for clients that are joining
+	leave   chan *client     //for leaving clients
+	clients map[*client]bool //list of curr clients
 }
 
-// forward is a channel that holds incoming messages
-// that should be forwarded to the other clients.
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for c := range r.clients {
+				c.send <- msg
+			}
+		}
+	}
+}
