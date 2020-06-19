f7b1502cc16f1c1a932adffe21d3d88a6eab9a8b
Rohan <rohanali@live.com>
2020-06-19 13:45:04 -0700

    Configurable Host

--- Chat Application with Web Sockets/chatApp.go
+++ Chat Application with Web Sockets/chatApp.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -16,23 +18,28 @@ type templateHandler struct {
 
 // ServeHTTP handles the HTTP request for the template.
 func (temp *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if temp.templ == nil {
+	temp.once.Do(func() {
+
 		temp.templ = template.Must(template.ParseFiles(filepath.Join("templates", temp.filename)))
-	}
-	temp.templ.Execute(w, nil)
+	})
+	temp.templ.Execute(w, r)
 
 }
 
 func main() {
 
+	var address = flag.String("address", ":3000", "The address of the Application")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "index.html"})
 	http.Handle("/room", r)
 
 	go r.run()
 
+	fmt.Println("Running the web server on port", *address)
+
 	// start the web servers
-	if err := http.ListenAndServe(":3000", nil); err != nil { //Defining Port
+	if err := http.ListenAndServe(*address, nil); err != nil { //Defining Port
 		log.Fatal("ListenAndServe:", err)
 	}
 }
