412f2042d5bef8e1534a191f6540f5370eff8f29
Rohan <rohanali@live.com>
2020-06-21 20:41:40 -0700

    Integrated OAuth for fb/google/github

--- Chat Application/chatApp.go
+++ Chat Application/chatApp.go
@@ -11,6 +11,10 @@ import (
 	"sync"
 
 	"./trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -33,6 +37,15 @@ func main() {
 
 	var address = flag.String("address", ":3000", "The address of the Application")
 	flag.Parse()
+	gomniauth.SetSecurityKey("PUT YOUR AUTH KEY HERE")
+	gomniauth.WithProviders(
+		facebook.New("2280011405478753", "48f54df6a418426cdc0e406e58344f66",
+			"http://localhost:3000/auth/callback/facebook"),
+		github.New("d22f0f061dc89517a0bb", "a642fca1fb25147fdc031b252c20205cea77f6f5",
+			"http://localhost:3000/auth/callback/github"),
+		google.New("799263709705-97dol722coqkcdoopo2uonm6u3cvhlaf.apps.googleusercontent.com", "zOjNIk7GMHRHHMrWzkKBgCeL",
+			"http://localhost:3000/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
